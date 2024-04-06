750b450214d01a3526c02fb662687ee10b5a2c5a
hiroky1983 <hirockysan1983@gmail.com>
2024-04-06 21:12:33 +0900

    sb

---
+++ front/components/client/top/Top.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { TopClient } from ".";
+
+const meta: Meta<typeof TopClient> = {
+  component: TopClient,
+};
+export default meta;
+
+type Story = StoryObj<typeof TopClient>;
+
+export const Default: Story = {
+  parameters: {
+    nextjs: {
+      router: {
+        pathname: "/about",
+      },
+    },
+  },
+};
